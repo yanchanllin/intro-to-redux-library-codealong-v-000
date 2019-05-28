5c2841aab0384410fdffe09d98b32d2fbae9deef
Yan C Lin <yonalin1@gmail.com>
2019-05-28 20:48:25 +0000

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,9 +6,14 @@ export default function shoppingListItemReducer(
 ) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+		console.log('Current state.items length %s', state.items.length);
+					 console.log('Updating state.items length to %s', state.items.length + 1);
+					 return Object.assign({}, state, {
+							 items: state.items.concat(state.items.length + 1)
+					 });
 
-		default:
-			return state;
+			 default:
+					 console.log('Initial state.items length: %s', state.items.length);
+					  return state;
 	}
 }
