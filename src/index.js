8914fa4f93b06f814450017ae8c780cd59032ff4
Yan C Lin <yonalin1@gmail.com>
2019-05-28 19:42:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,4 +10,9 @@ import './index.css';
 
 const store = createStore(shoppingListItemReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}
+  {''}
+  <App /> 
+  </Provider> 
+  document.getElementById('root'));
